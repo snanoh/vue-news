a950b0c4260bd939286663a784454d9a82c7f57c
User <njs1324@gmail.com>
2021-12-02 21:28:17 +0900

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView';
+import AskView from '../views/AskView';
+import JobsView from '../views/JobsView';
+
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  history: true,
+  routes: [
+    {
+      path: '/',
+      redirect: '/news'
+    },
+    {
+      path: '/news',
+      component: NewsView,
+    },
+    {
+      path: '/ask',
+      component: AskView,
+    },
+    {
+      path: '/jobs',
+      component: JobsView,
+    }
+  ]
+})
