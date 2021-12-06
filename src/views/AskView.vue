a8c24291e4b0eed02f6a773c497366d5e27032f8
User <njs1324@gmail.com>
2021-12-06 22:24:56 +0900

    css 적용

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-      <p v-for="item in fetchedAsk">
-        <router-link v-bind:to="`/item/${item.id}`">
-          {{ item.title }}
-        </router-link>
-        <small>{{ item.time_ago }} by {{ item.user }}</small>
-      </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedAsk" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link v-bind:to="`/item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by 
+            <router-link class="link-text" v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,6 +39,30 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list{
+  padding-left: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+.post{
+  border-bottom: 1px solid #eee;
+  align-items: center;
+  list-style: none;
+  display: flex;
+}
+.points {
+      display: flex;
+    align-items: center;
+    width: 80px;
+    height: 60px;
+    justify-content: center;
+    color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text{
+  color: #828282
+}
 </style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-      <p v-for="job in this.$store.state.jobs">
-        <a :href="job.url">
-          {{ job.title }}
-        </a>
-        <small>{{ job.time_ago }} by {{ job.domain }}</small>
-      </p>
+    <ul class="news-list">
+      <li v-for="job in this.$store.state.jobs" class="post">
+        <div class="points">
+          {{ job.points || 0 }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="job.url">
+              {{ job.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by 
+            <a :href="job.url">
+              {{ job.domain }}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,6 +33,30 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  padding-left: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+.post{
+  border-bottom: 1px solid #eee;
+  align-items: center;
+  list-style: none;
+  display: flex;
+}
+.points {
+      display: flex;
+    align-items: center;
+    width: 80px;
+    height: 60px;
+    justify-content: center;
+    color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text{
+  color: #828282
+}
 </style>
